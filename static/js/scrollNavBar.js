0d4ec209991aac9a8c00e1fbd8b6728ad89f9229
Clovis <diclovis@gmail.com>
2017-09-03 13:35:51 -0500

    V_1.010, 2017-09-03, deployment

---
+++ static/js/scrollNavBar.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function() {
+
+  $('.main').css('margin-top', $("#animation").height()-$(".head").height() +50);
+
+  var myTimeout;
+  $('.media-body').mouseenter(function() {
+    // myTimeout = setTimeout(function() {
+      $('.media-body>h1').text("Clovis Djiometsa Ngnitewe").hide();
+      $('.media-body>h1').show();
+      // }, 500);
+    }).mouseleave(function() {
+        // clearTimeout(myTimeout);
+        $('.media-body>h1').html("<code>{</code>Clovis_Djiometsa<code>}</code>").hide();
+        $('.media-body>h1').show();
+      });
+
+  var cssImgOn = {
+    width: 250,
+    transition: 'width 1s ease'
+  };
+  var cssImgOff = {
+    width: 150,
+    transition: 'width 1s ease'
+  };
+  $('.media-left>#clo').hover(function(){
+    $(this).css(cssImgOn);
+  }, function() {
+    $(this).css(cssImgOff);
+  })
+
+  $( window ).resize(function() {
+    if( $(window).width() ) {
+      $('canvas').css("width", $(window).width());
+      $('canvas').css("height", $(window).height()*2/5);
+    }
+  }).resize();
+
+  $('#myMap,#duomo,#montreal,#austin').click(function() {
+    $(this).parent().removeClass('col-md-6').removeClass('col-md-offset-6').addClass('col-md-12');
+  }).mouseleave(function() {
+    $(this).parent().addClass('col-md-6').addClass('col-md-offset-6');
+  })
+
+  $('#myMap').mouseenter(function() {
+    $(this).parent().removeClass('col-md-6').removeClass('col-md-offset-6').addClass('col-md-12');
+  }).mouseleave(function() {
+    $(this).parent().addClass('col-md-6').addClass('col-md-offset-6');
+  })
+})
