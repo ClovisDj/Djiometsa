dd73fa137fd358215206fd6d57a4c17dc6b4be59
clovis <diclovis@gmail.com>
2021-01-10 10:23:34 -0600

    New year 2021 updates

--- resume/static/js/scrollNavBar.js
+++ resume/static/js/scrollNavBar.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
   })
 
 
-  $('#myMap,#duomo,#montreal,#austin, #clev, #greenv').click(function() {
+  $('#myMap,#duomo,#montreal,#austin, #clev, #greenv, #silicon-hills-img').click(function() {
     $(this).parent().removeClass('col-md-6').removeClass('col-md-offset-6').addClass('col-md-12');
   }).mouseleave(function() {
     $(this).parent().addClass('col-md-6').addClass('col-md-offset-6');
